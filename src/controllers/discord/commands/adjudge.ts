91617d20cd595b56e175df89a166d0b069259a5e
ford-narongrit <narongrit.th@ku.th>
2023-08-12 11:54:00 +0700

    add adjude

---
+++ src/controllers/discord/commands/adjudge.ts
@@ -0,0 +1,69 @@
+import * as Discord from 'discord.js';
+import { Command } from '../command';
+import { vote } from '../utils/vote';
+import { player } from '../utils/player';
+import { move_to } from '../utils/move_to';
+
+export const Adjudge: Command = {
+  name: 'adjudge',
+  description: 'took someone to attitude adjustment',
+  options: [
+    {
+      name: 'user',
+      description: 'user, who do you want to take to attitude adjustment',
+      type: Discord.ApplicationCommandOptionType.User,
+      required: true,
+    },
+    {
+      name: 'in',
+      description: 'channel, where you want to take to attitude adjustment',
+      type: Discord.ApplicationCommandOptionType.Channel,
+      required: true,
+      channel_types: [Discord.ChannelType.GuildVoice],
+    },
+    {
+      name: 'url',
+      description: 'youtube url',
+      type: Discord.ApplicationCommandOptionType.String,
+      required: true,
+    },
+  ],
+  run: async (client: Discord.Client, interaction: Discord.CommandInteraction) => {
+    try {
+      // user that use command
+      const commander: any = interaction.member;
+      if (!commander) return;
+
+      // Get options value.
+      const user = interaction.options.getMember('user') as Discord.GuildMember;
+      const channel_in = interaction.options.get('in')?.channel as Discord.VoiceChannel;
+      const url = interaction.options.get('url')?.value as string;
+      const members_count = commander.voice.channel.members.size;
+
+      // return missing options.
+      if (!user || !channel_in || !url || !interaction.channel || !interaction.guild) {
+        const content = 'error command missing some options.';
+        await interaction.followUp({
+          ephemeral: true,
+          content,
+        });
+        return;
+      }
+
+      vote(client, interaction, members_count, 10000, async () => {
+        // move user to channel.
+        const member = await move_to(user, channel_in.id);
+        // play song.
+        await player(interaction, url, channel_in.id);
+      });
+
+      const content = 'JUDGEMENT DAY!';
+      await interaction.followUp({
+        ephemeral: true,
+        content,
+      });
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  },
+};
--- src/controllers/discord/commands/index.ts
+++ src/controllers/discord/commands/index.ts
@@ -2,5 +2,6 @@ import { Command } from '../command';
 import { Hello } from './hello';
 import { Wake } from './wake';
 import { Play } from './play';
+import { Adjudge } from './adjudge';
 
-export const Commands: Command[] = [Hello, Wake, Play];
+export const Commands: Command[] = [Hello, Wake, Play, Adjudge];
